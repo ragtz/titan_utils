ee91f8e56731f8ecbbb618b94dbdfc097c40c7d6
Jordan Schneider <joschnei@cs.utexas.edu>
2019-09-11 15:00:00 -0500

    Add script for submitting jobs to titan cluster.

---
+++ submit-titan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Identifies the best titan-machine to run a command on and executes that command remotely. Aliases must live in .bashrc in order to be respected. Best GPU should be identified by the python script.
+results=$(python titan_utils.py)
+IFS=$'\n'; results=($results); unset IFS;
+best_titan=${results[0]}
+echo "Executing \`${@}\` on ${best_titan}"
+ssh -t $USER@$best_titan "/bin/bash -ci \"${@}\""
