52e78521e31a22bc34396d2540b4ae255e8e4d8d
Jordan Schneider <joschnei@cs.utexas.edu>
2019-09-12 15:24:07 -0500

    Runs script from current pwd.

--- submit-titan.sh
+++ submit-titan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Identifies the best titan-machine to run a command on and executes that command remotely. Aliases must live in .bashrc in order to be respected. Best GPU should be identified by the python script.
-results=$(python titan_utils.py)
+results=$(python ~/titan_utils/titan_utils.py)
 IFS=$'\n'; results=($results); unset IFS;
 best_titan=${results[0]}
+loc=$(pwd)
 echo "Executing \`${@}\` on ${best_titan}"
-ssh -t $USER@$best_titan "/bin/bash -ci \"${@}\""
+ssh -t $USER@$best_titan "/bin/bash -ci \"cd ${loc}; ${@}\""
